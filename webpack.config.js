7effa91e5c2ed6060e13ca0a2e7c5cf3a3d6f097
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2022-01-25 15:31:10 +0300

    delete redirect, change webpack

--- webpack.config.js
+++ webpack.config.js
@@ -35,9 +35,6 @@ module.exports = (env, argv) => {
       new HtmlWebpackPlugin({
         template: './src/index.html',
       }),
-      new CopyPlugin({
-        patterns: [{ from: '_redirects', to: '' }],
-      }),
     ],
     devServer: {
       hot: true,
